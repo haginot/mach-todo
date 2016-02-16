f6a288380978b9b81bb22b8d5018a747ad46fa6f
takahiro-hagino <takahiro.hagino@bizreach.co.jp>
2016-02-16 22:36:56 +0900

    Initialize Play project

---
+++ mach-todo/app/controllers/TaskController.scala
@@ -0,0 +1,15 @@
+package controllers
+
+import javax.inject.Inject
+
+import play.api.mvc.Controller
+
+class TaskController @Inject() extends Controller {
+
+  def list = TODO
+
+  def create = TODO
+
+  def delete = TODO
+
+}
