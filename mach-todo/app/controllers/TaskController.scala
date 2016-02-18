1391939057db0dccab81e2bdc983fc54e9ce9199
takahiro-hagino <takahiro.hagino@bizreach.co.jp>
2016-02-18 22:49:09 +0900

    WIP adding REST API Sample

--- mach-todo/app/controllers/TaskController.scala
+++ mach-todo/app/controllers/TaskController.scala
@@ -2,14 +2,33 @@ package controllers
 
 import javax.inject.Inject
 
-import play.api.mvc.Controller
+import play.api.libs.json.Json
+import play.api.mvc.{Action, Controller}
 
 class TaskController @Inject() extends Controller {
 
-  def list = TODO
+  implicit val task = Json.writes[TaskController.Task]
+
+  def list = Action { implicit rc =>
+
+    Ok(Json.toJson(TaskController.tasks))
+  }
 
   def create = TODO
 
   def delete(taskId: Int) = TODO
+
+}
+
+object TaskController {
+
+  case class Task(id: Long, title: String, status: String)
+
+  val tasks = List(
+    Task(1, "Do something", "DONE"),
+    Task(2, "Do something else", "DONE"),
+    Task(3, "Do more stuff", "NOTDONE"),
+    Task(4, "Do that again", "NOTDONE")
+  )
 
 }
